0e496936b3ed65911d6119fcf46b117a97a924b0
Yaqiu Shi <spratumn@163.com>
2023-03-29 10:03:01 +0800

    fix: imshow image too small

--- mark_tool.py
+++ mark_tool.py
@@ -28,19 +28,6 @@ def load_image(image_path):
         image = cv2.imread(image_path)
     return image
 
-def read_html_nodes(html_path):
-    with open(html_path, 'r') as html_file:
-        table_nodes = re.split(r'<h4></h4>\n<table>\n', html_file.read())
-
-    table_dict = {}
-    for table_node in table_nodes[1:]:
-        user_line, lib_line = table_node.split('\n')[1:3]
-        user_id = user_line[4:-5]
-        lib_id = lib_line[4:-5]
-        table_id = f'{user_id}-{lib_id}'
-        if table_id not in table_dict:
-            table_dict[table_id] = '<h4></h4>\n<table>\n' + table_node
-    return table_dict
 
 def read_html_nodes(html_path):
     with open(html_path, 'r') as html_file:
@@ -49,6 +36,8 @@ def read_html_nodes(html_path):
     table_dict = {}
     for table_node in table_nodes[1:]:
         table_node_lines = table_node.split('\n')
+        idx_in_all = table_node_lines[1][4:-5]
+        score = table_node_lines[10][4:9]
         user_line, lib_line = table_node_lines[5:7]
         user_id = user_line[4:-5]
         lib_id = lib_line[4:-5]
@@ -58,7 +47,7 @@ def read_html_nodes(html_path):
         user_path = user_path_line.split(' ')[1].split('=')[1]
         lib_path = lib_path_line.split(' ')[1].split('=')[1]
         if table_id not in table_dict:
-            table_dict[table_id] = [user_path, lib_path]
+            table_dict[table_id] = [user_path, lib_path, idx_in_all, score]
     return table_dict
 
 
@@ -103,67 +92,72 @@ def read_csv_results(csv_path):
     return results
 
 
-def mark(window_size=(2000, 1000)):
+def mark(window_size=(1000, 1000)):
     COLORMAPS = [
         (0, 255, 0),
         (0, 255, 255),
         (0, 0, 255)
     ]
-    cv2.namedWindow('mark', cv2.WINDOW_NORMAL)
+
     table_dict = read_html_nodes('test.html')
     results = read_csv_results('test.csv')
 
     result_dict = {}
     test_length = len(results)
     idx = 0
-    split_width = 4
     while 1:
         user_id ,lib_id, test_idx, cur_label = results[idx]
         table_id = f'{user_id},{lib_id}'
         if table_id not in table_dict:
             idx += 1
             continue
-        user_path, lib_path = table_dict[table_id]
+        user_path, lib_path, idx_in_all, score = table_dict[table_id]
         if table_id in result_dict: cur_label = result_dict[table_id]
 
-        user_image = load_image(user_path)
-        lib_image = load_image(lib_path)
-        user_height, user_width = user_image.shape[:2]
-        lib_height, lib_width = lib_image.shape[:2]
-        w_scale = (user_width + lib_width) / (window_size[0] - split_width)
-        if w_scale <= 1: w_scale = 1
-
-        user_image = resize_image(user_image, (int(user_width / w_scale), int(user_height / w_scale)))
-        lib_image = resize_image(lib_image, (int(lib_width / w_scale), int(lib_height / w_scale)))
+        user_image = resize_image(load_image(user_path), window_size)
+        lib_image = resize_image(load_image(lib_path), window_size)
 
         user_h, user_w = user_image.shape[:2]
         lib_h, lib_w = lib_image.shape[:2]
 
-        mark_image_height = max(user_h, lib_h)
-        mark_image_width = window_size[0]
-        mark_image = np.ones((mark_image_height, mark_image_width, 3), np.uint8)
+        mark_user_image = np.zeros((window_size[1], window_size[0], 3), np.uint8)
+        mark_lib_image = np.zeros((window_size[1], window_size[0], 3), np.uint8)
+        user_left = (window_size[0] - user_w) // 2
+        user_top = (window_size[1] - user_h) // 2
+        mark_user_image[user_top:user_top+user_h, user_left:user_left+user_w] = user_image
 
-
-        user_left = max((mark_image_width - user_w - lib_w - split_width) // 2, 0)
-        user_right = user_left + user_w
-        user_top = max((mark_image_height - user_h) // 2, 0)
-        mark_image[user_top:user_top+user_h, user_left:user_right] = user_image
-
-        lib_left = (mark_image_width - user_right - lib_w) // 2 + user_right
-        lib_top = max((mark_image_height - lib_h) // 2, 0)
-        mark_image[lib_top:lib_top+lib_h, lib_left:lib_left+lib_w] = lib_image
+        lib_left = (window_size[0] - lib_w) // 2
+        lib_top = (window_size[1] - lib_h) // 2
+        mark_lib_image[lib_top:lib_top+lib_h, lib_left:lib_left+lib_w] = lib_image
 
         if cur_label != -1:
-            cv2.putText(mark_image, str(cur_label),
-                        (mark_image_width // 2 - 180, mark_image_height // 2 + 50),
+            cv2.putText(mark_user_image, str(cur_label),
+                        (window_size[0] // 2 - 180, window_size[1] // 2 + 50),
                         cv2.FONT_HERSHEY_SIMPLEX,
-                        15, COLORMAPS[cur_label], 15)
-        cv2.putText(mark_image, f'{idx+1}/{test_length}',
+                        7, COLORMAPS[cur_label], 5)
+            cv2.putText(mark_lib_image, str(cur_label),
+                        (window_size[0] // 2 - 180, window_size[1] // 2 + 50),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        7, COLORMAPS[cur_label], 5)
+        cv2.putText(mark_user_image, f'{idx+1}/{test_length}',
                         (100, 100),
                         cv2.FONT_HERSHEY_SIMPLEX,
-                        2, (0, 255, 255), 5)
+                        2, (0, 255, 255), 2)
+        cv2.putText(mark_user_image, idx_in_all,
+                        (100, 200),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        2, (0, 255, 255), 2)
+        cv2.putText(mark_lib_image, f'{idx+1}/{test_length}',
+                        (100, 100),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        2, (0, 255, 255), 2)
+        cv2.putText(mark_lib_image, score,
+                        (100, 200),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        2, (0, 255, 255), 2)
         while 1:
-            cv2.imshow('mark', mark_image)
+            cv2.imshow('mark_usr', mark_user_image)
+            cv2.imshow('mark_lib', mark_lib_image)
             key = cv2.waitKey(0)
             if key in (27, 48, 49, 50, 97, 100, 111):
                 break
@@ -198,15 +192,11 @@ def save_results(results, result_dict, result_path):
     return results
 
 
-
-
 def compare_with_xlsx(xlsx_path_1, xlsx_path_2):
     data1 = pd.read_excel(xlsx_path_1)
     print(f'read {xlsx_path_1}, has {data1.shape[0]} rows, {data1.shape[1]} columns.')
     data2 = pd.read_excel(xlsx_path_2)
     print(f'read {xlsx_path_2}, has {data2.shape[0]} rows, {data2.shape[1]} columns.')
-
-
     for i in range(1, 1151):
         user_id = data1.iat[i, 0]
         lib_id = data1.iat[i, 1]
@@ -235,6 +225,7 @@ def split_xlsx(ori_xlsx_path):
         with open(sub_csv_path, 'w') as f:
             f.write('\n'.join(total_lines))
 
+
 def download_image_cache():
     table_dict = read_html_nodes('test.html')
     results = read_csv_results('test.csv')
@@ -246,6 +237,7 @@ def download_image_cache():
         load_image(user_path)
         load_image(lib_path)
 
+
 if __name__ == '__main__':
     if not os.path.exists(IMAGE_CACHE_DIR):
         os.mkdir(IMAGE_CACHE_DIR)
@@ -253,7 +245,7 @@ if __name__ == '__main__':
     # download_image_cache()
     # split_xlsx('test.xlsx')
     mark()
-    # compare_with_xlsx('jiu.xlsx', 'test_1.xlsx')
+    # compare_with_xlsx('test.xlsx', 'test_5.xlsx')
     # while 1:
     #     cv2.imshow('', np.zeros((100, 100)))
     #     print(cv2.waitKey())
